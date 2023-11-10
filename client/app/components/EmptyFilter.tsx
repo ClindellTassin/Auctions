5ec8333fc596dd66028c4434ca2f21641943931d
ClindellTassin <clindelljtassin@gmail.com>
2023-11-10 09:27:31 -0600

    Setup Client Side Authentication

--- client/app/components/EmptyFilter.tsx
+++ client/app/components/EmptyFilter.tsx
@@ -1,14 +1,21 @@
+'use client'
+
 import { useParamsStore } from "@/hooks/useParamsStore";
 import Heading from "./Heading";
 import { Button } from "flowbite-react";
+import { signIn } from "next-auth/react";
 
 type Props = {
     title?: string;
     subtitle?: string;
     showReset?: boolean;
+    showLogin?: boolean;
+    callbackUrl?: string;
 };
 
-const EmptyFilter = ({ title = 'No matches for this filter', subtitle = 'Try changing or reseting the filter', showReset }: Props) => {
+const EmptyFilter = (
+    { title = 'No matches for this filter', subtitle = 'Try changing or reseting the filter', showReset, showLogin, callbackUrl }:
+        Props) => {
     const reset = useParamsStore(state => state.reset);
 
     return (
@@ -17,6 +24,9 @@ const EmptyFilter = ({ title = 'No matches for this filter', subtitle = 'Try cha
             <div className='mt-4'>
                 {showReset && (
                     <Button outline onClick={reset}>Remove Filters</Button>
+                )}
+                {showLogin && (
+                    <Button outline onClick={() => signIn('id-server', { callbackUrl })}>Login</Button>
                 )}
             </div>
         </div>
--- client/app/nav/Logo.tsx
+++ client/app/nav/Logo.tsx
@@ -1,13 +1,21 @@
 'use client'
 
 import { useParamsStore } from "@/hooks/useParamsStore"
+import { usePathname, useRouter } from "next/navigation";
 import { AiOutlineCar } from "react-icons/ai"
 
 const Logo = () => {
     const reset = useParamsStore(state => state.reset);
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const doReset = () => {
+        if (pathname !== '/') router.push('/');
+        reset();
+    };
 
     return (
-        <div onClick={reset} className='flex items-center gap-2 text-3xl font-semibold text-red-500 cursor-pointer'>
+        <div onClick={doReset} className='flex items-center gap-2 text-3xl font-semibold text-red-500 cursor-pointer'>
             <AiOutlineCar size={34} />
             <div>Carsties Auctions</div>
         </div>
